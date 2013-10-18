d55559d903e67b22c9ebfe7abb712754a48a365e
Shahid <Shahid@Shahids-MacBook-Pro.local>
2013-10-18 21:42:10 +0200

    Adding maps js in application.rb file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require gmaps4rails/gmaps4rails.base.js.coffee
+//= require gmaps4rails/gmaps4rails.bing.js.coffee
+//= require gmaps4rails/gmaps4rails.googlemaps.js.coffee
+//= require gmaps4rails/gmaps4rails.base.js
+//= require gmaps4rails/gmaps4rails.mapquest.js.coffee
+//= require gmaps4rails/gmaps4rails.openlayers.js.coffee
 //= require_tree .
